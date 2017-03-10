f72b7a78bde36db50eb5eebdba34dc40572bce17
Pat Holland <pholls@users.noreply.github.com>
2017-03-10 14:01:45 -0800

    replace goldenrod css class with yellow

--- app/assets/javascripts/game_view.js
+++ app/assets/javascripts/game_view.js
@@ -6,12 +6,12 @@ GameView.prototype.update = function(model){
   flatten(model.board).forEach(function(cellValue, index){
     if(cellValue === '0'){
       $("#cell-" + index).text('');
-      $("#cell-" + index).removeClass('blue', 'goldenrod').addClass('gray');
+      $("#cell-" + index).removeClass('blue', 'yellow').addClass('gray');
     } else if(cellValue >= 256) {
-      $("#cell-" + index).removeClass('blue', 'gray').addClass('goldenrod');
+      $("#cell-" + index).removeClass('blue', 'gray').addClass('yellow');
     } else {
       $("#cell-" + index).text(cellValue);
-      $("#cell-" + index).removeClass('gray', 'goldenrod').addClass('blue');
+      $("#cell-" + index).removeClass('gray', 'yellow').addClass('blue');
     }
   });
 }
